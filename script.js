87bd514ea8708df644ccbfc9191e5777cac821c8
codingisfornerdyninjas <128403544+codingisfornerdyninjas@users.noreply.github.com>
2023-11-28 10:56:38 -0600

    s

---
+++ script.js
@@ -0,0 +1,6 @@
+
+    document.getElementById("hc").addEventListener("click" , function(){
+     setTimeout(function(){
+       document.getElementById("hcc").style.display = "block"
+     } , 1000)
+    })
