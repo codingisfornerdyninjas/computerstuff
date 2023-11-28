cb108f9d4a5ecf64e7956bbf0582ad2b9d3c64d4
codingisfornerdyninjas <128403544+codingisfornerdyninjas@users.noreply.github.com>
2023-11-28 11:01:25 -0600

    ss

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-
-    document.getElementById("hc").addEventListener("click" , function(){
-     setTimeout(function(){
-       document.getElementById("hcc").style.display = "block"
-       alert("TEST")
-     } , 1000)
-    })
+document.addEventListener("DOMContentLoaded" , function(){
+alert()
+   
+})
