6587f66f34d1454b5bf1ab341cfcf069446db00e
codingisfornerdyninjas <128403544+codingisfornerdyninjas@users.noreply.github.com>
2023-11-28 10:58:26 -0600

    sssss

--- script.js
+++ script.js
@@ -2,5 +2,6 @@
     document.getElementById("hc").addEventListener("click" , function(){
      setTimeout(function(){
        document.getElementById("hcc").style.display = "block"
+       alert("TEST")
      } , 1000)
     })
