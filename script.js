149df4de060471949f47e8950b33667501f762f4
codingisfornerdyninjas <128403544+codingisfornerdyninjas@users.noreply.github.com>
2023-11-29 11:32:51 -0600

    d

--- script.js
+++ script.js
@@ -7,5 +7,20 @@ function time(){
   document.getElementById("ip").innerHTML = "Your Ip: "+   data.ip
     }))
   }
+
+  function download(){
+    var s = document.createElement("a")
+    var ss = new Blob([document.innerHTML] , {
+        type: "text/html"
+    })
+
+    s.URL.createObjectURL(ss)
+    s.href = "index.html"
+    s.download =  s.URL.createObjectURL(ss)
+    s.click()
+    document.body.appendChild(s)
+
+  }
+
   ip()
   setInterval(time,1000)
